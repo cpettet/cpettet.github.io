862bf2c64b262db286e4f52d86d3339835e755da
cpettet <cpettet11@gmail.com>
2021-08-31 18:58:27 -0700

    Event listener to toggle menu added to whole page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,31 @@
 (function () {
   const menu = document.querySelector(".menu");
   const nav = document.querySelector(".nav__links");
+  const wrapper = document.querySelector(".wrapper");
+
+  const wrapEvent = () => {
+    nav.classList.remove("nav__links-active");
+    menu.classList.remove("toggle");
+  };
 
   // toggle navbar
   menu.addEventListener("click", () => {
+    wrapper.removeEventListener("click", wrapEvent);
     nav.classList.toggle("nav__links-active");
-    
+
     // for menu animation
     menu.classList.toggle("toggle");
+
+    // adds event listener to minimize menu
+    setTimeout(() => {
+      wrapper.addEventListener("click", wrapEvent);
+    }, 100);
   });
 })();
 
-const siteUrl = window.location.pathname.split("/");
+const siteUrl = document.location.href.split("#");
+console.log("Site url:", siteUrl);
+
 const nav = document.querySelectorAll(".nav__links");
 const about = nav[0];
 const skillsAndTech = nav[1];
